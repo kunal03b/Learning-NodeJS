40c7010da3db1e44e3b58a1e0bd9331bedc6f6af
KUNAL SHARMA <kunal03b@gmail.com>
2023-04-26 21:54:35 +0530

    created a simple web server

--- demo.js
+++ demo.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const http = require('http');
 
 // // blocking synchronous way
 // const textIn = fs.readFileSync('F:/Learning NodeJS/input.txt', 'utf-8');
@@ -9,16 +10,27 @@ const fs = require('fs');
 // console.log('File Written :)');
 
 
-// Non blocking asynchronous way
-fs.readFile('F:/Learning NodeJS/input.txt', 'utf-8', (err, data1) => {
-    console.log(data1);
-    fs.readFile(`F:/Learning NodeJS/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2);
+// // Non blocking asynchronous way
+// fs.readFile('F:/Learning NodeJS/inputtt.txt', 'utf-8', (err, data1) => {
+//     if(err) return console.log('Error occured');
+//     console.log(data1);
+//     fs.readFile(`F:/Learning NodeJS/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
 
-        fs.writeFile('F:/Learning NodeJS/final.txt', `${data2}\n${data1}`, 'utf-8', err => {
-            console.log('File has been written successfully');
-        });
-        console.log(data2);
-    })
+//         fs.writeFile('F:/Learning NodeJS/final.txt', `${data2}\n${data1}`, 'utf-8', err => {
+//             console.log('File has been written successfully');
+//         });
+//         console.log(data2);
+//     })
+// });
+// console.log('Just checking what will happen...');
+
+// -------------------------------------------------------------------------------------------------------
+// Creating a simple web server
+const server = http.createServer((req,res) => {
+    res.end('Hello! From the server');
 });
-console.log('Just checking what will happen...');
+
+server.listen(4000,'127.0.0.1',() => {
+    console.log('Listening to requests on port 4000.');
+})
